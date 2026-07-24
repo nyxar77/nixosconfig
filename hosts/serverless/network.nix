04091b8677fb8340c4311bf5a247399a15240a9f
fe2-Nyxar <nyxar49@gmail.com>
2026-07-24 13:48:52 +0100

    Use encrypted DNS on serverless

--- hosts/serverless/network.nix
+++ hosts/serverless/network.nix
@@ -1,12 +1,45 @@
 {...}: {
   programs.mtr.enable = true;
 
+  services.dnscrypt-proxy = {
+    enable = true;
+    upstreamDefaults = false;
+    settings = {
+      listen_addresses = ["127.0.0.1:53"];
+      server_names = [
+        "cloudflare-family"
+        "quad9-filtered"
+      ];
+
+      ipv4_servers = true;
+      ipv6_servers = false;
+      dnscrypt_servers = false;
+      doh_servers = true;
+      odoh_servers = false;
+      require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = false;
+      ignore_system_dns = true;
+      netprobe_address = "1.1.1.1:443";
+
+      cache = true;
+      cache_size = 4096;
+
+      static = {
+        cloudflare-family.stamp = "sdns://AgMAAAAAAAAABzEuMC4wLjMABzEuMC4wLjMKL2Rucy1xdWVyeQ";
+        quad9-filtered.stamp = "sdns://AgMAAAAAAAAABzkuOS45LjkgsBkgdEu7dsmrBT4B4Ht-BQ5HPSD3n3vqQ1-v5DydJC8SZG5zOS5xdWFkOS5uZXQ6NDQzCi9kbnMtcXVlcnk";
+      };
+    };
+  };
+
   networking = {
     hostName = "serverless";
 
     networkmanager = {
-      enable = false;
+      enable = true;
       dns = "none";
+      wifi.backend = "iwd";
+      wifi.powersave = false;
     };
 
     wireless.iwd = {
@@ -27,10 +60,7 @@
       ];
     };
 
-    nameservers = [
-      "9.9.9.9"
-      "94.140.14.14"
-    ];
+    nameservers = ["127.0.0.1"];
 
     defaultGateway = {
       address = "192.168.1.1";
