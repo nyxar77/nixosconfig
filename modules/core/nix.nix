2b0adc6c2ff17da06186e458829d3f5e7ead2dde
fe2-Nyxar <nyxar49@gmail.com>
2026-07-24 13:37:12 +0100

    Clean up Nix store settings

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,30 +1,32 @@
-{ lib, ... }: {
+{lib, ...}: {
   # nixpkgs.config.allowUnfree = true;
-  # Allow experimental feature "flakes"
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
 
   nix = {
     gc = {
       automatic = true;
+      dates = "weekly";
+      persistent = true;
+
       options = lib.mkDefault "--delete-older-than 15d";
     };
     optimise.automatic = true;
-  };
-  nix.settings = {
-    auto-optimise-store = true;
-    substituters = [
-      "https://cache.nixos.org/"
-      "https://hyprland.cachix.org"
-      # "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      # "nix-community.cachix.org-1:mB9kiyNJG0XrqcnihK6kQyE3XG8qV7yLs8uYe41rP2Q="
-    ];
+    settings = {
+      auto-optimise-store = true;
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://hyprland.cachix.org"
+        # "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        # "nix-community.cachix.org-1:mB9kiyNJG0XrqcnihK6kQyE3XG8qV7yLs8uYe41rP2Q="
+      ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
   };
   /*
      system = {
