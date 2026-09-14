314777a6f38cf38ea3682987d63dcebeef32acc6
fe2-Nyxar <nyxar49@gmail.com>
2026-09-14 21:21:02 +0100

    enable key-only SSH for serverless

--- hosts/serverless/users.nix
+++ hosts/serverless/users.nix
@@ -1,7 +1,14 @@
 {
+  services.openssh.settings = {
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
+    PermitRootLogin = "no";
+  };
+
   users.users.baryon = {
     isNormalUser = true;
     useDefaultShell = true;
     extraGroups = ["wheel"];
+    openssh.authorizedKeys.keyFiles = [ ../../files/serverless.pub ];
   };
 }
