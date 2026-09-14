41ecadda8598ae70618df692a26693e95f27882b
fe2-Nyxar <nyxar49@gmail.com>
2026-09-14 21:23:20 +0100

    clean up maintenance services

--- modules/core/maintenance.nix
+++ modules/core/maintenance.nix
@@ -1,22 +1,15 @@
 {
   # enable firmware updates
-  services.fwupd.enable = true;
-
-  services.openssh = {
-    enable = true;
-    /*
-    extraConfig = ''
-      Host serverless
-        HostName 192.168.1.51
-        User baryon
-        IdentityFile /home/nyxar/.ssh/serverless
+  services = {
+    fwupd.enable = true;
+    openssh = {
+      enable = true;
+    };
+    journald.extraConfig = ''
+      SystemMaxUse=300M
+      MaxRetentionSec=14day
     '';
-    */
   };
-  services.journald.extraConfig = ''
-    SystemMaxUse=300M
-    MaxRetentionSec=14day
-  '';
 
   systemd.coredump.settings.Coredump = {
     MaxUse = "100M";
