fb040bf7a0cbbf9ff9103ba3103ba09cc6dbd32f
fe2-Nyxar <nyxar49@gmail.com>
2026-09-14 21:21:02 +0100

    update Syncthing folder configuration

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -4,78 +4,81 @@
   ...
 }:
 lib.mkIf config.nyx.services.syncthing.enable {
-  services = {
-    syncthing = {
-      enable = true;
-      user = "nyxar";
-      dataDir = "/home/nyxar";
-      configDir = "/home/nyxar/.config/syncthing";
-      openDefaultPorts = true;
-      guiPasswordFile = config.sops.secrets.syncthing-gui-password.path;
+  services =
+    let
+      userDir = "/home/nyxar";
+      devices = [
+        "reno4"
+        "Nothing"
+      ];
+    in
+    {
+      syncthing = {
+        enable = true;
+        user = "nyxar";
+        dataDir = userDir;
+        configDir = "${userDir}/.config/syncthing";
+        openDefaultPorts = true;
+        guiPasswordFile = config.sops.secrets.syncthing-gui-password.path;
 
-      settings = {
-        gui = {
-          user = "nyxar";
-        };
-        devices = {
-          "reno4" = {
-            id = "5O3OEE6-3RNKLDB-ZHYKZ3G-D6AXIUY-6V2M3TU-VW32CSH-RNPGJ6F-LERTJAN";
+        settings = {
+          gui = {
+            user = "nyxar";
           };
-          "oppo8" = {
-            id = "CW7QULU-4C4KCKH-ZOPCZK6-7WYI3WS-26BGYWF-UZHY5PA-UJYACHG-UNXHOQU";
+          devices = {
+            "reno4" = {
+              id = "5O3OEE6-3RNKLDB-ZHYKZ3G-D6AXIUY-6V2M3TU-VW32CSH-RNPGJ6F-LERTJAN";
+            };
+            "oppo8" = {
+              id = "CW7QULU-4C4KCKH-ZOPCZK6-7WYI3WS-26BGYWF-UZHY5PA-UJYACHG-UNXHOQU";
+            };
+            "Nothing" = {
+              id = "PBV6J2C-SO5UDW7-LEZGYP3-PPZIJVI-C5Y3UET-COISGYN-67LTTFY-CSMGZAV";
+            };
           };
-          "Nothing" = {
-            id = "PBV6J2C-SO5UDW7-LEZGYP3-PPZIJVI-C5Y3UET-COISGYN-67LTTFY-CSMGZAV";
-          };
-        };
-        folders = let
-          userDir = "/home/nyxar";
-          devices = [
-            "reno4"
-            # "oppo8"
-            "Nothing"
-          ];
-        in {
-          "keepass" = {
-            id = "xkarx-qj2wy";
-            path = "${userDir}/Documents/KeePass/Group1/";
-            inherit devices;
-          };
-          "music" = {
-            id = "Music-spo";
-            path = "${userDir}/Music/spotify/";
-            inherit devices;
-          };
-          "pfp" = {
-            id = "pfp";
-            path = "${userDir}/Pictures/Images/pfp";
-            inherit devices;
-          };
-          "The_Bp" = {
-            id = "Bp-ms";
-            path = "${userDir}/Documents/syncthing/The-Bp/";
-            inherit devices;
-          };
-          "Configs" = {
-            id = "global-config";
-            path = "${userDir}/Documents/syncthing/global-Configs/";
-            inherit devices;
-          };
-          "Twitter" = {
-            id = "Twitter-sync";
-            path = "${userDir}/Documents/syncthing/Twitter/";
-            inherit devices;
-          };
-          "books" = {
-            id = "books-sync";
-            path = "${userDir}/Documents/syncthing/books";
-          };
-          "Notes" = {
-            id = "Notes-sync";
-            path = "${userDir}/Documents/Notes";
+          folders = {
+            "keepass" = {
+              id = "xkarx-qj2wy";
+              path = "${userDir}/Documents/KeePass/Group1/";
+              inherit devices;
+            };
+            "music" = {
+              id = "Music-spo";
+              path = "${userDir}/Music/spotify/";
+              inherit devices;
+            };
+            "pfp" = {
+              id = "pfp";
+              path = "${userDir}/Pictures/Images/pfp";
+              inherit devices;
+            };
+            "The_Bp" = {
+              id = "Bp-ms";
+              path = "${userDir}/Documents/syncthing/The-Bp/";
+              inherit devices;
+            };
+            "Configs" = {
+              id = "global-config";
+              path = "${userDir}/Documents/syncthing/global-Configs/";
+              inherit devices;
+            };
+            "Twitter" = {
+              id = "Twitter-sync";
+              path = "${userDir}/Documents/syncthing/Twitter/";
+              inherit devices;
+            };
+            "books" = {
+              id = "books-sync";
+              path = "${userDir}/Documents/syncthing/books";
+              inherit devices;
+            };
+            "Notes" = {
+              id = "Notes-sync";
+              path = "${userDir}/Documents/Notes";
+
+            };
           };
         };
       };
     };
-  };
 }
